af9378cfa408f0351c08e6bd691f844d0660e615
TribusK2 <biuro@tribusk2.pl>
2019-06-22 20:51:58 +0200

    Add searching function

--- app/app.js
+++ app/app.js
@@ -4,10 +4,18 @@ myApp.controller('aplCtrl', ['$scope', '$http', function($scope, $http){
     $http.get('data/aplications.json').then(function(response){
         $scope.aplications = response.data;
     })
+
+    // sort data
     $scope.order = 'id_apl';
     $scope.reverse = false;
     $scope.sortBy = function(order) {
         $scope.reverse = ($scope.order === order) ? !$scope.reverse : false;
         $scope.order = order;
-      };
+    };
+    
+    // search data
+    $scope.search = '';
+    $scope.filterBy = function(person) {
+        return (person.id_apl + person.email + person.first_name.toLowerCase() + person.last_name.toLowerCase() + person.create_time + person.update_time + person.comment.toLowerCase()).indexOf($scope.search.toLowerCase()) > -1;
+    };
 }]);
