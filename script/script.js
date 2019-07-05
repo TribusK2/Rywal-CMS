6a6e0a60161d08fe0906c3ad1f58019f74127fe0
TribusK2 <biuro@tribusk2.pl>
2019-07-05 18:27:53 +0200

    Add submit animation

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
-// $(document).ready(function(){
-    
-// });
+$(document).ready(function(){
+    // load animation
+    $("#loader").fadeOut(400, function(){
+        $("#page").fadeIn(400);
+    });
+        
+    $("#logForm").submit(function(event) {
+        var form = this;
+        event.preventDefault();             // prevent submit
+        $("#page").fadeOut(400, function(){ // amination
+            form.submit();                  // submit when amination done
+        });
+    });
+    // load animation end
+});
